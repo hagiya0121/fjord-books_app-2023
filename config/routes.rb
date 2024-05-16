090b88a2e6f974a9fa910620b324b36e64dbff22
hagiya0121 <koazu0121@gmail.com>
2024-05-16 19:10:03 +0900

    feat: 画像のアップロードと表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
   root to: 'books#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :books
   resources :users, only: %i(index show)
 end
