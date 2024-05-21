12362b55db0d47869c0bb78b63abc45578588eed
hagiya0121 <koazu0121@gmail.com>
2024-05-22 03:00:57 +0900

    レビューの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # rubocop:disable Rails/LexicallyScopedActionFilter
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-  # rubocop:enable Rails/LexicallyScopedActionFilter
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_sign_up_params
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[
                                         postal_code address self_introduction
                                       ])
-  end
-
-  def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[
                                         id postal_code address self_introduction
                                       ])
