b690902df39e1949cfbcf8e676f553ae94487f3f
hagiya0121 <koazu0121@gmail.com>
2024-05-23 01:44:52 +0900

    レビューの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[
-                                        postal_code address self_introduction
-                                      ])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[
-                                        id postal_code address self_introduction
-                                      ])
+    permitted_params = %i[postal_code address self_introduction]
+    devise_parameter_sanitizer.permit(:sign_up, keys: permitted_params)
+    devise_parameter_sanitizer.permit(:account_update, keys: permitted_params)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def allowed_user?(user)
-    current_user.id == user.id
-  end
 end
