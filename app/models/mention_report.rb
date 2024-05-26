f3a4850ac5932332242543c5b182ee2d108059ac
hagiya0121 <koazu0121@gmail.com>
2024-05-26 16:08:05 +0900

    feat: モデルを作成

---
+++ app/models/mention_report.rb
@@ -0,0 +1,6 @@
+class MentionReport < ApplicationRecord
+  belongs_to :mentioning_report, class_name: 'Report'
+  belongs_to :mentioned_report, class_name: 'Report'
+
+  validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
+end
