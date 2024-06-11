6e9db07295e122efe6e577ad296f9f6da6bb33ca
hagiya0121 <koazu0121@gmail.com>
2024-06-11 18:50:45 +0900

    test: Reportモデルのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @alice = users(:alice)
+    @report_by_alice = reports(:report_by_alice)
+    @report_by_bob = reports(:report_by_bob)
+  end
+
+  test '自分の日報を編集できる' do
+    assert @report_by_alice.editable?(@alice)
+  end
+
+  test '他人の日報を編集できない' do
+    assert_not @report_by_bob.editable?(@alice)
+  end
+
+  test '日報作成日時をDateオブジェクトに変換できる' do
+    assert_instance_of Date, @report_by_alice.created_on
+  end
 end
