a93a5a001579b1bfe538e5b240c0cd55e089abba
hagiya0121 <koazu0121@gmail.com>
2024-06-14 20:27:06 +0900

    test: Reportモデルのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -20,4 +20,10 @@ class ReportTest < ActiveSupport::TestCase
   test '日報作成日時をDateオブジェクトに変換できる' do
     assert_instance_of Date, @report_by_alice.created_on
   end
+
+  test 'メンションした日報が保存される' do
+    @report_by_alice.content = "http://localhost:3000/reports/#{@report_by_bob.id}"
+    @report_by_alice.save
+    assert_equal @report_by_bob, @report_by_alice.mentioning_reports.first
+  end
 end
