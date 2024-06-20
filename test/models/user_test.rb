93fa29de4153d912867deb31d8af37b6c505c220
hagiya0121 <koazu0121@gmail.com>
2024-06-21 04:35:54 +0900

    test: テストするメゾッド名を明示

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,11 +7,11 @@ class UserTest < ActiveSupport::TestCase
     @alice = users(:alice)
     @user_without_name = users(:user_without_name)
   end
-  test 'ユーザー名が設定されているときはその名前を返す' do
+  test 'name_or_email はユーザー名が設定されているときはその名前を返す' do
     assert_equal 'alice', @alice.name_or_email
   end
 
-  test 'ユーザー名が設定されていないときはメールアドレスを返す' do
+  test 'name_or_email はユーザー名が設定されていないときはメールアドレスを返す' do
     assert_equal 'user_without_name@example.com', @user_without_name.name_or_email
   end
 end
